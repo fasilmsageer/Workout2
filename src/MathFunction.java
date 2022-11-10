201c6be0ae116b1d121a250b1ef0838e14ab175c
fasilmsageer <fasilmsageer@gmail.com>
2022-11-10 16:36:54 +0530

    commit

--- src/MathFunction.java
+++ src/MathFunction.java
@@ -1,32 +1,22 @@
-import java.util.Scanner;
-
-public class MathFunction
-{
+public class MathFunction {
+    static float multiply (int a, int b)
+    {
+        return (a*b);
+    }
+    static float multiply (float a, float b)
+    {
+        return (a*b);
+    }
+    static float multiply(float a,int b)
+    {
+        return (a*b);
+    }
     public static void main(String[] args)
     {
-        int a, b, p1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 2 integers: ");
-        a=sc.nextInt();
-        b=sc.nextInt();
-        p1=a*b;
-        System.out.println("the product of 2 integer values entered is: "+p1);
+        System.out.println(multiply(3, 2));
 
-        double c,d,p2;
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Enter 2 float values: ");
-        c= sc1.nextDouble();
-        d= sc1.nextDouble();
-        p2=c*d;
-        System.out.println("the product of 2 float values entered is: "+p2);
+        System.out.println(multiply(3.1f, 2.1f));
 
-        int e;
-        double f,p3;
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("Enter an integer and a float value ");
-        e= sc2.nextInt();
-        f= sc2.nextDouble();
-        p3=e*f;
-        System.out.println("the product of an integer and float is: "+p3);
+        System.out.println(multiply(2.1f, 4));
     }
 }
